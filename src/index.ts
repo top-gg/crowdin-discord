cfb4a8a7959e459ce38015faa7c383ceb766c115
Julian Berryessa <jb@jt3ch.net>
2021-05-18 16:22:08 -0400

    Fixes to formatting

--- src/index.ts
+++ src/index.ts
@@ -8,19 +8,22 @@ async function handleRequest(request: Request) {
   const url = new URL(request.url)
   const res = await request.json()
   const eventsStructured = groupBy(res.events, event => event.event)
-  let query = request.query
-  let stringAddAmount; if (eventsStructured.hasOwnProperty('string.added')) {stringAddAmount = JSON.stringify(eventsStructured["string.added"].length)} else {stringAddAmount = 0}
-  let stringChangeAmount; if (eventsStructured.hasOwnProperty('string.updated')) {stringChangeAmount = JSON.stringify(eventsStructured["string.updated"].length)} else {stringChangeAmount = 0}
-  let stringDeleteAmount; if (eventsStructured.hasOwnProperty('string.deleted')) {stringDeleteAmount = JSON.stringify(eventsStructured["string.deleted"].length)} else {stringDeleteAmount = 0}
-  const body = JSON.stringify({
-    content: `New changes were made in project!\n${stringAddAmount} lines added.\n${stringChangeAmount} lines changed.\n${stringDeleteAmount} lines deleted.`
-  })
 
-let discordWebhook = url.searchParams.get('webhook_link')
+  const stringAdded = eventsStrctures['string.added']?.length || 0
+  const stringUpdated = eventsStrctures['string.updated']?.length || 0
+  const stringDeleted = eventsStrctures['string.deleted']?.length || 0
+
+  const discordWebhook = url.searchParams.get('webhook_link')
   await fetch(discordWebhook, {
     method: "POST",
     headers: { 'Content-type': 'application/json' },
-    body
+    body: JSON.stringify({
+      content: 
+        `New changes were made in project!\n` +
+        `${stringAdded} lines added.\n` +
+        `${stringUpdated} lines changed.\n`
+        `${stringDeleted} lines deleted.`
+    })
   })
 
   return new Response('OK')
