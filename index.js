c7e6ee60094ea0ba4ed9ad6bb618620356bada26
Pacmard <030704@mail.ru>
2021-05-18 19:54:52 +0300

    init

---
+++ index.js
@@ -0,0 +1,25 @@
+addEventListener('fetch', event => {
+  event.respondWith(handleRequest(event.request))
+})
+
+async function handleRequest(request) {
+const url = new URL(request.url)
+let project; 
+if (url.pathname == "/backend" || request.method == "POST") {
+  project = 'https://crwd.in/topgg-entities'
+} else if (url.pathname == "/main" || request.method == "POST"){ 
+    project = 'https://crwd.in/topgg'
+}
+
+const body = JSON.stringify({
+  content: `New lines were added!\n${project}`
+})
+
+await fetch('DISCORD WEBHOOK URL', {
+  method: "POST",
+  headers: { 'Content-type': 'application/json' },
+  body
+})
+
+return new Response("OK")
+}
