80ae7dac6333d8c34a1f1bbacc076e7f147156ed
Pacmard <030704@mail.ru>
2021-05-18 23:02:26 +0300

    fin

--- src/index.ts
+++ src/index.ts
@@ -6,15 +6,21 @@ addEventListener('fetch', event => {
 
 async function handleRequest(request: Request) {
   const url = new URL(request.url)
-  let project
-  if (url.pathname == '/backend' || request.method == 'POST') {
-    project = 'https://crwd.in/topgg-entities'
-  } else if (url.pathname == '/main' || request.method == 'POST') {
-    project = 'https://crwd.in/topgg'
-  }
-
+  const res = await request.json()
+  const eventsStructured = groupBy(res.events, event => event.event)
+  let query = request.query
+  let stringAddAmount; if (eventsStructured.hasOwnProperty('string.added')) {stringAddAmount = JSON.stringify(eventsStructured["string.added"].length)} else {stringAddAmount = 0}
+  let stringChangeAmount; if (eventsStructured.hasOwnProperty('string.updated')) {stringChangeAmount = JSON.stringify(eventsStructured["string.updated"].length)} else {stringChangeAmount = 0}
+  let stringDeleteAmount; if (eventsStructured.hasOwnProperty('string.deleted')) {stringDeleteAmount = JSON.stringify(eventsStructured["string.deleted"].length)} else {stringDeleteAmount = 0}
   const body = JSON.stringify({
-    content: `New lines were added!\n${project}`,
+    content: `New changes were made in project!\n${stringAddAmount} lines added.\n${stringChangeAmount} lines changed.\n${stringDeleteAmount} lines deleted.`
+  })
+
+let discordWebhook = url.searchParams.get('webhook_link')
+  await fetch(discordWebhook, {
+    method: "POST",
+    headers: { 'Content-type': 'application/json' },
+    body
   })
 
   return new Response('OK')
